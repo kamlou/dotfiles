35ccda193d9f8ccad508d105f641a7e0c8bfc496
Lou Kamenov <kamlou@defx.org>
2014-10-15 23:31:25 -0400

    move acme specific bits to the wrapper

--- profile.d/plan9.sh
+++ profile.d/plan9.sh
@@ -1,18 +1,15 @@
-FONT=/mnt/font/Verdana/13a/font
-ACME_DUMP=$HOME/acme.dump
 tabstop=4
 EDITOR="9 F"
 P9P=$(9 sh -c 'echo $PLAN9')
 
 # we need this so we can use osx fonts in acme, install if it doesn't exist
 [[ -e $P9P/bin/fontsrv ]] || (cd $P9P/src/cmd/fontsrv && 9 mk install)
-[[ ! -f $HOME/acme.dump ]] || touch $HOME/acme.dump
 
 # launch plumber
-pgrep -q plumber || plumber
+pgrep -q plumber || 9 plumber
 [[ -d $HOME/lib ]] || mkdir -p $HOME/lib
 9 9p read plumb/rules | sed 's/^editor\=.*/editor=acme2/g' > $HOME/lib/plumber.$$
 9 9p write plumb/rules < $HOME/lib/plumber.$$
 rm -f $HOME/lib/plumber.$$
 
-export tabstop font EDITOR P9P FONT ACME_DUMP
+export tabstop font EDITOR P9P
